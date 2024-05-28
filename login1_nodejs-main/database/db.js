ab7d68c050673ce0e0552dbf46e76f30adaf65a4
daniel-sanchez9 <128751842+daniel-sanchez9@users.noreply.github.com>
2024-05-28 17:49:22 -0500

    2
    2

--- login1_nodejs-main/database/db.js
+++ login1_nodejs-main/database/db.js
@@ -1,19 +1,18 @@
-//2 - Invocamos a MySQL y realizamos la conexion
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
-    //Con variables de entorno
     host     : 'sql104.infinityfree.com',
     user     : 'if0_36633976',
     password : 'sSrWeqhvc8sDo',
     database : 'if0_36633976_login_node_curso'
 });
 
-connection.connect((error)=>{
+connection.connect((error) => {
     if (error) {
-      console.error('El error de conexión es: ' + error);
-      return;
+        console.error('El error de conexión es: ' + error);
+        return;
     }
     console.log('¡Conectado a la Base de Datos!');
-  });
+});
 
-  module.exports = connection;
+module.exports = connection;
